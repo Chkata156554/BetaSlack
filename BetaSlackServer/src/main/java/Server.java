de16e34d5f0c0e37522abf72ff4eda8a2e2c1567
Kata <catachudob@gmail.com>
2019-03-02 14:51:23 +0100

    Daily update

--- BetaSlackServer/src/main/java/Server.java
+++ BetaSlackServer/src/main/java/Server.java
@@ -4,20 +4,42 @@ import java.net.ServerSocket;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Server {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
-    private BufferedReader input;
+    private BufferedReader clientInput;
     private PrintWriter output;
+    private static final String EXIT_KEYWORD = "exit";
 
     public void startServer (int port) throws IOException{
         serverSocket = new ServerSocket(port);
         System.out.println("Server pending...");
         clientSocket = serverSocket.accept();
         System.out.println("Client has been connected.");
-        input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        while(!clientSocket.isClosed()) {
+            String receivedMessage = "";
+
+            try {
+                receivedMessage = clientInput.readLine();
+            } catch(SocketException ex) {
+                System.out.println("Client has been disconnected");
+                break;
+            }
+
+            if(receivedMessage.toLowerCase().equals(EXIT_KEYWORD)) {
+                break;
+            }
+
+            System.out.println(receivedMessage);
+        }
+
+        /**input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         output = new PrintWriter(clientSocket.getOutputStream(), true);
         output.println("Hello from server");
         System.out.println(input.readLine());
@@ -25,7 +47,7 @@ public class Server {
 
 
 
-        /**while(clientSocket.isClosed() && !clientSocket.isInputShutdown()){
+        while(clientSocket.isClosed() && !clientSocket.isInputShutdown()){
         }
         System.out.println("Client has disconnected.");
          input = new BufferedReader(new InputStreamReader(socket.getInputStream()))
